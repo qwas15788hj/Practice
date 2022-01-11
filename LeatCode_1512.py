2b492283fa1e54d58bcf015656a873cbf2924fc0
qwas15788@gmail.com <201621179@sangmyung.kr>
2022-01-11 21:33:19 +0900

    22.01.11

---
+++ LeatCode_1512.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        nums = nums
+        count = 0
+        
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+                
+        return count
