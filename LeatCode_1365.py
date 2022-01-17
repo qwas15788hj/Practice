72c22519fa28b15a0c83cd78bddf025b40fa0cce
qwas15788@gmail.com <qwas15788@gmail.com>
2022-01-17 23:34:30 +0900

    22.01.17

---
+++ LeatCode_1365.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        list = []
+        
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if nums[i] > nums[j]:
+                    count += 1
+            list.append(count)
+            
+        return list
