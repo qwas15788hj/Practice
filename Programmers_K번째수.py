ed2ee4095800aa40ec7c5400151adeda3eba0c7d
qwas15788@gmail.com <qwas15788@gmail.com>
2022-02-03 22:55:26 +0900

    22.02.03

---
+++ Programmers_K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        a = []
+        for j in range(commands[i][0]-1, commands[i][1]):
+            a.append(array[j])
+            a.sort()
+        answer.append(a[commands[i][2]-1])
+        
+    return answer
