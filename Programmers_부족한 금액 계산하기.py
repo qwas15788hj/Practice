33faa146f70215267a2bb541662a300850bddbf7
qwas15788@gmail.com <qwas15788@gmail.com>
2022-02-07 23:10:44 +0900

    22.02.07

---
+++ Programmers_부족한 금액 계산하기.py
@@ -0,0 +1,9 @@
+def solution(price, money, count):
+    result = 0
+    for i in range(1, count+1):
+        result += price*i
+    
+    if money >= result:
+        return 0
+    else:
+        return abs(money-result)
